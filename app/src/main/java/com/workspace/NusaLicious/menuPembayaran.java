62f7be10f7c704ecbbd86fa7403a19366a7f1a8d
VIPWorkSpace <vipworkspace20@gmail.com>
2020-05-23 15:03:33 +0700

    Desain riwayat pesanan

---
+++ app/src/main/java/com/workspace/NusaLicious/menuPembayaran.java
@@ -0,0 +1,16 @@
+package com.workspace.NusaLicious;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.workspace.societybeta.R;
+
+public class menuPembayaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_pembayaran);
+    }
+}
